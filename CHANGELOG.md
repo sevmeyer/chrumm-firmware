c8ed0ea204c65292a633008902cf992304e03255
Severin Meyer <hello@sev.dev>
2020-07-10 21:16:08 +0200

    Simplify events and key indexing

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+## 0.1.1 (2020-07-10)
+
+- Simplified press and release functions.
+- Replaced 2D key matrix with 1D key list.
+- Detecting layer key by index instead of reserved key code.
+
 ## 0.1.0 (2020-07-02)
 
 - Initial release.
--- chrumm/chrumm.ino
+++ chrumm/chrumm.ino
@@ -1,4 +1,4 @@
-// Chrumm keyboard firmware 0.1.0
+// Chrumm keyboard firmware 0.1.1
 // https://github.com/sevmeyer/chrumm-firmware
 //   ____ _   _ ____  _   _ _    _ _    _
 //  / ___| |_| |  _ \| | | | \  / | \  / |
@@ -24,76 +24,52 @@ const byte colCount = 14;
 const byte rowPins[rowCount] = {A5, A4, A3, A2, A1, A0};
 const byte colPins[colCount] = {12, 11, 10, 9, 7, 5, 3, 2, 1, 0, 4, 6, 8, 14};
 
-const KeyCode layerKey = 0xFF;
-const KeyCode layers[][rowCount][colCount] = {
+const byte layerKeyIndex = 3*colCount + 0;
+const KeyCode keys[2][rowCount*colCount] = {
 {
 	// Primary layer
-	{kEsc    ,kF1  ,kF2   ,kF3    ,kF4   ,kF5   ,kNone,kF6   ,kF7   ,kF8   ,kF9    ,kF10   ,kF11   ,kF12   },
-	{kTilde  ,k1   ,k2    ,k3     ,k4    ,k5    ,kF20 ,k6    ,k7    ,k8    ,k9     ,k0     ,kMinus ,kEqual },
-	{kTab    ,kQ   ,kW    ,kE     ,kR    ,kT    ,kF21 ,kY    ,kU    ,kI    ,kO     ,kP     ,kLBrace,kRBrace},
-	{layerKey,kA   ,kS    ,kD     ,kF    ,kG    ,kF22 ,kH    ,kJ    ,kK    ,kL     ,kColon ,kQuote ,kBSlash},
-	{kLShift ,kZ   ,kX    ,kC     ,kV    ,kB    ,kUp  ,kN    ,kM    ,kComma,kPeriod,kSlash ,kRShift,kNone  },
-	{kNone   ,kLAlt,kSpace,kBSpace,kLCtrl,kLeft ,kDown,kRight,kRCtrl,kEnter,kSpace ,kRAlt  ,kNone  ,kNone  }
-},{
+	kEsc   ,kF1  ,kF2   ,kF3    ,kF4   ,kF5   ,kNone,kF6   ,kF7   ,kF8   ,kF9    ,kF10   ,kF11   ,kF12   ,
+	kTilde ,k1   ,k2    ,k3     ,k4    ,k5    ,kF20 ,k6    ,k7    ,k8    ,k9     ,k0     ,kMinus ,kEqual ,
+	kTab   ,kQ   ,kW    ,kE     ,kR    ,kT    ,kF21 ,kY    ,kU    ,kI    ,kO     ,kP     ,kLBrace,kRBrace,
+	kNone  ,kA   ,kS    ,kD     ,kF    ,kG    ,kF22 ,kH    ,kJ    ,kK    ,kL     ,kColon ,kQuote ,kBSlash,
+	kLShift,kZ   ,kX    ,kC     ,kV    ,kB    ,kUp  ,kN    ,kM    ,kComma,kPeriod,kSlash ,kRShift,kNone  ,
+	kNone  ,kLAlt,kSpace,kBSpace,kLCtrl,kLeft ,kDown,kRight,kRCtrl,kEnter,kSpace ,kRAlt  ,kNone  ,kNone
+},
+{
 	// Secondary layer
-	{kNone   ,kF13 ,kF14  ,kF15   ,kF16  ,kF17  ,kNone,kF18  ,kF19  ,kpDiv ,kpMul  ,kpSub  ,kF23   ,kF24   },
-	{kNone   ,kNone,kNone ,kPrint ,kScrLk,kPause,kNone,kNone ,kp7   ,kp8   ,kp9    ,kpAdd  ,kNone  ,kNone  },
-	{kNone   ,kNone,kNone ,kNone  ,kNone ,kNone ,kNone,kNone ,kp4   ,kp5   ,kp6    ,kpAdd  ,kNone  ,kNone  },
-	{layerKey,kNone,kNone ,kInsert,kHome ,kPgUp ,kNone,kNone ,kp1   ,kp2   ,kp3    ,kpEnter,kNone  ,kNone  },
-	{kNone   ,kNone,kNone ,kDelete,kEnd  ,kPgDn ,kUp  ,kNone ,kp0   ,kp0   ,kpDot  ,kpEnter,kNone  ,kNone  },
-	{kNone   ,kNone,kSpace,kBSpace,kNone ,kLeft ,kDown,kRight,kNone ,kEnter,kSpace ,kNone  ,kNone  ,kNone  }
+	kNone  ,kF13 ,kF14  ,kF15   ,kF16  ,kF17  ,kNone,kF18  ,kF19  ,kpDiv ,kpMul  ,kpSub  ,kF23   ,kF24 ,
+	kNone  ,kNone,kNone ,kPrint ,kScrLk,kPause,kNone,kNone ,kp7   ,kp8   ,kp9    ,kpAdd  ,kNone  ,kNone,
+	kNone  ,kNone,kNone ,kNone  ,kNone ,kNone ,kNone,kNone ,kp4   ,kp5   ,kp6    ,kpAdd  ,kNone  ,kNone,
+	kNone  ,kNone,kNone ,kInsert,kHome ,kPgUp ,kNone,kNone ,kp1   ,kp2   ,kp3    ,kpEnter,kNone  ,kNone,
+	kNone  ,kNone,kNone ,kDelete,kEnd  ,kPgDn ,kUp  ,kNone ,kp0   ,kp0   ,kpDot  ,kpEnter,kNone  ,kNone,
+	kNone  ,kNone,kSpace,kBSpace,kNone ,kLeft ,kDown,kRight,kNone ,kEnter,kSpace ,kNone  ,kNone  ,kNone
 }};
 
 
 // Events
 // ------
 
-// The secondary layer is active as long as the layer key
-// is pressed. To prevent unintended key events, layers
-// can only change after all other keys are released.
+byte activeLayer = 0;
 
-byte activeLayer   = 0;
-byte intendedLayer = 0;
-byte pressedKeys   = 0;
-
-void press(byte row, byte col)
+void press(byte index)
 {
-	const KeyCode key = layers[activeLayer][row][col];
-
-	switch (key) {
-		case kNone:
-			return;
-		case layerKey:
-			intendedLayer = 1;
-			break;
-		default:
-			BootKeyboard.press(key);
-			++pressedKeys;
-			break;
+	if (index == layerKeyIndex)
+		activeLayer = 1;
+	else {
+		BootKeyboard.add(keys[activeLayer][index]);
+		BootKeyboard.send();
 	}
-
-	if (pressedKeys == 0)
-		activeLayer = intendedLayer;
 }
 
-void release(byte row, byte col)
+void release(byte index)
 {
-	const KeyCode key = layers[activeLayer][row][col];
-
-	switch (key) {
-		case kNone:
-			return;
-		case layerKey:
-			intendedLayer = 0;
-			break;
-		default:
-			BootKeyboard.release(key);
-			--pressedKeys;
-			break;
+	if (index == layerKeyIndex)
+		activeLayer = 0;
+	else {
+		BootKeyboard.remove(keys[0][index]);
+		BootKeyboard.remove(keys[1][index]);
+		BootKeyboard.send();
 	}
-
-	if (pressedKeys == 0)
-		activeLayer = intendedLayer;
 }
 
 
@@ -106,17 +82,17 @@ void release(byte row, byte col)
 const byte debounceFlip = 5;
 const byte debounceMax  = 10;
 
-byte debounceCounts[rowCount][colCount];
+byte debounceCounts[rowCount*colCount];
 
-void debounce(byte row, byte col, byte signal)
+void debounce(byte index, byte signal)
 {
-	byte& count = debounceCounts[row][col];
+	byte& count = debounceCounts[index];
 
 	if (signal == 0) {
 		if (count > 0) {
 			if (--count == debounceFlip) {
 				count = 0;
-				press(row, col);
+				press(index);
 			}
 		}
 	}
@@ -124,7 +100,7 @@ void debounce(byte row, byte col, byte signal)
 		if (count < debounceMax) {
 			if (++count == debounceFlip) {
 				count = debounceMax;
-				release(row, col);
+				release(index);
 			}
 		}
 	}
@@ -145,9 +121,8 @@ void setup()
 	for (byte col = 0; col < colCount; ++col)
 		digitalWrite(colPins[col], HIGH);
 
-	for (byte row = 0; row < rowCount; ++row)
-		for (byte col = 0; col < colCount; ++col)
-			debounceCounts[row][col] = debounceMax;
+	for (byte index = 0; index < rowCount*colCount; ++index)
+		debounceCounts[index] = debounceMax;
 
 	BootKeyboard.begin();
 }
@@ -178,7 +153,7 @@ void loop()
 		digitalWrite(colPins[col], LOW);
 
 		for (byte row = 0; row < rowCount; ++row)
-			debounce(row, col, digitalRead(rowPins[row]));
+			debounce(row*colCount + col, digitalRead(rowPins[row]));
 
 		digitalWrite(colPins[col], HIGH);
 	}
